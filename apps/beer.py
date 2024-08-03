0ff30b91daaa9832e061a94f7d21a37ad792ab4d
Matsuuu <huhta.matias@gmail.com>
2024-08-03 12:49:29 +0300

    Get some beer info

--- apps/beer.py
+++ apps/beer.py
@@ -3,10 +3,11 @@ import jpegdec
 import os
 from badger2040 import WIDTH
 import urequests
+import json
 
-f = open("/state/beer.json", "r")
-f.write("Testing")
-f.close()
+from service.brewfather import get_batch_info, get_batches
+
+print("Initializing beer.py")
 
 TEXT_SIZE = 1
 LINE_HEIGHT = 15
@@ -30,55 +31,55 @@ RIGHT_PANE = SCREEN_SIZE - PADDING - LEFT_PANE
 
 RIGHT_PANE_START = LEFT_PANE + PADDING
 
+beer = get_batch_info("z6G844ONYxUVUg2s4PJpUpMh8Tv4Xb")
+print("BEER ")
+print(beer)
+
 # Screen = 296x128
 
+def display_beer_info(beer):
+    display.set_font("bitmap8")
+    display.set_pen(0)
+    display.rectangle(48, 0, WIDTH, 32)
 
-display.set_font("bitmap8")
-display.set_pen(0)
-display.rectangle(48, 0, WIDTH, 32)
+    icon = "/apps/icon-beer.jpg"
+    jpeg.open_file(icon)
+    jpeg.decode(0, 0)
 
-icon = "/apps/icon-beer.jpg"
-jpeg.open_file(icon)
-jpeg.decode(0, 0)
+    display.set_pen(15)
+    beer_label = "System Shock"
 
-display.set_pen(15)
-beer_label = "System Shock"
+    title_x_offset = 52
+    display.text(beer_label, title_x_offset, 8, WIDTH, TEXT_SIZE * 2)
 
-title_x_offset = 52
-display.text(beer_label, title_x_offset, 8, WIDTH, TEXT_SIZE * 2)
+    display.set_pen(0)
 
-display.set_pen(0)
+    content_x_offset = 60
+    current_y = 40
 
-content_x_offset = 60
-current_y = 40
+    display.set_font("bitmap6")
 
-display.set_font("bitmap6")
+    display.text("West Coast IPA", content_x_offset, current_y, WIDTH, TEXT_SIZE * 2)
+    current_y += LINE_HEIGHT
 
-display.text("West Coast IPA", content_x_offset, current_y, WIDTH, TEXT_SIZE * 2)
-current_y += LINE_HEIGHT
+    display.text("7.1% abv.", content_x_offset, current_y, WIDTH, TEXT_SIZE * 2)
+    current_y += LINE_HEIGHT
 
-display.text("7.1% abv.", content_x_offset, current_y, WIDTH, TEXT_SIZE * 2)
-current_y += LINE_HEIGHT
-
-display.text("60 IBU", content_x_offset, current_y, WIDTH, TEXT_SIZE * 2)
-current_y += LINE_HEIGHT * 2
-
-
-display.set_font("bitmap8")
-display.set_thickness(4)
-
-display.text("Amarillo, Cascade, Simcoe, Loral", content_x_offset, current_y, WIDTH - content_x_offset, TEXT_SIZE)
-current_y += LINE_HEIGHT
-
-display.text("Matsu Brewing", content_x_offset, current_y, WIDTH - content_x_offset, TEXT_SIZE)
-current_y += LINE_HEIGHT
+    display.text("60 IBU", content_x_offset, current_y, WIDTH, TEXT_SIZE * 2)
+    current_y += LINE_HEIGHT * 2
 
 
-URL = ""
+    display.set_font("bitmap8")
+    display.set_thickness(4)
 
-r = urequests.get(URL)
-# open the json data
-j = r.json()
+    display.text("Amarillo, Cascade, Simcoe, Loral", content_x_offset, current_y, WIDTH - content_x_offset, TEXT_SIZE)
+    current_y += LINE_HEIGHT
+
+    display.text("Matsu Brewing", content_x_offset, current_y, WIDTH - content_x_offset, TEXT_SIZE)
+    current_y += LINE_HEIGHT
+
+    batches = get_batches()
+    print(batches['batches'][0]['brewer'])
 
 # display.text("info", WIDTH - display.measure_text("help", 0.4) - 4, 4, WIDTH, 1)
 
@@ -102,6 +103,7 @@ j = r.json()
 
 display.update()
 
+print("[beer.py]: Display updated")
 # Call halt in a loop, on battery this switches off power.
 # On USB, the app will exit when A+C is pressed because the launcher picks that up.
 while True:
--- service/brewfather.py
+++ service/brewfather.py
@@ -1,13 +1,49 @@
-import base64
 import badger2040
+import urequests
+from BREWFATHER_CONFIG import API_KEY, USER_ID
+import ubinascii
+import json
+
+BATCHES_URL = "https://api.brewfather.app/v2/batches?order_by=batchNo&order_by_direction=desc&include=recipe.style.name"
+BATCH_URL = "https://api.brewfather.app/v2/batches/"
+BATCH_URL_OPTIONS="?include=measuredAbv,measuredOg,measuredFg,estimatedIbu,recipe.name,recipe.style.name"
+RECIPES_URL = ""
 
 def get_brewfather_token():
-    config_file = open("/BREWFATHER_CONFIG.py", "r")
+    token = ubinascii.b2a_base64(USER_ID + ":" + API_KEY)
+    return token.decode("utf-8")
 
-get_brewfather_token()
 
-display = badger2040.Badger2040()
+def get_batches():
+    display = badger2040.Badger2040()
+    display.connect()
+    
+    r = urequests.get(BATCHES_URL, headers = { "authorization": "Basic " + get_brewfather_token() })
 
-while True:
-    display.keepalive()
-    display.halt()
+    res = r.text
+    fixed_json_string = "{ \"batches\": " + res + "}"
+
+    batches = json.loads(fixed_json_string)
+    return batches
+
+def get_batch_info(batch_id):
+    display = badger2040.Badger2040()
+    display.connect()
+
+    request_url = BATCH_URL + batch_id + BATCH_URL_OPTIONS
+
+    r = urequests.get(request_url, headers = { "authorization": "Basic " + get_brewfather_token() })
+
+    json_data = r.json()
+
+    print(json_data)
+
+    return {
+        "id": json_data["_id"],
+        "brewer": json_data["brewer"],
+        "name": json_data["recipe"]["name"],
+        "abv": json_data.get("measuredAbv"),
+        "og": json_data.get("measuredOg"),
+        "fg": json_data.get("measuredFg"),
+        "ibu": json_data.get("estimatedIbu")
+    }
