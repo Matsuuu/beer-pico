5243296ba7d8ee7fef69186db432460decfd8990
Matsuuu <huhta.matias@gmail.com>
2024-08-06 21:10:36 +0300

    Listing & menu stuff

--- apps/beer_listing.py
+++ apps/beer_listing.py
@@ -1,6 +1,6 @@
 from service.brewfather import get_batches
 import badger2040
-from badger2040 import WIDTH
+from badger2040 import HEIGHT, WIDTH
 
 from badger_util import clear_screen
 
@@ -8,12 +8,15 @@ from badger_util import clear_screen
 display = badger2040.Badger2040()
 display.set_update_speed(badger2040.UPDATE_FAST)
 
+# Approximate center lines for buttons A, B and C
+centers = (41, 147, 253)
+
 class BeerListing():
 
     batches = []
     current_page = 0
     current_selected_item = 0;
-    items_per_page = 8
+    items_per_page = 7
 
     def __init__(self):
         clear_screen(display)
@@ -24,6 +27,7 @@ class BeerListing():
         clear_screen(display)
         y = 4
         LINE_HEIGHT = 15
+        # Draw menu items
         for i, batch in enumerate(self.batches):
             if self.current_selected_item == i:
                 display.set_pen(0)
@@ -34,6 +38,18 @@ class BeerListing():
                 display.set_pen(0)
                 display.text(batch.get("recipe", "<No Recipe>").get("name", "<No Name>"), 4, y)
             y += LINE_HEIGHT
+
+        # Draw instructions
+        menu_items = ["", "Info", "Select"]
+
+        display.set_pen(0)
+        display.rectangle(0, HEIGHT - 10, WIDTH, 10)
+        display.set_pen(15)
+
+        for i, menu_item in enumerate(menu_items):
+            text_width = display.measure_text(menu_item, 1)
+            display.text(menu_item, centers[i] - round(text_width / 2), HEIGHT - 8, 300, 1)
+
         display.update()
 
     def previous_item(self):
